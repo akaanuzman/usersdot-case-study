9c85b408c6e6f1d9f64c0ae5ac9413111e970947
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-09-29 23:07:38 +0300

    Refactor user table pagination, page size selection, and search functionality added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,39 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Input, Button, Modal, Tooltip, Tag, Row, Col, Typography, Select } from 'antd';
-import { EditOutlined, DeleteOutlined, UserAddOutlined } from '@ant-design/icons';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Table, Input, Button, Modal, Tooltip, Tag, Row, Col, Typography, Select, Empty } from 'antd';
+import { EditOutlined, DeleteOutlined, UserAddOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons';
+import UserModel from './models/user.model';
 
 const { Option } = Select;
+const { Search } = Input;
 
 const App = () => {
   const [users, setUsers] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
   const [searchTerm, setSearchTerm] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
 
-  useEffect(() => {
-    fetchData();
-  }, [currentPage, pageSize]);
-
-  const fetchData = async () => {
+  /**
+   * @description Fetch users from API
+   * @returns {Promise<void>}
+   * @throws {Error}
+   */
+  const fetchUsers = useCallback(async () => {
     try {
-      const response = await fetch(`http://localhost:3001/users?page=${currentPage}&pageSize=${pageSize}`);
+      const response = await fetch(`http://localhost:3001/users?page=${currentPage}&pageSize=${pageSize}&search=${searchTerm}`);
       if (!response.ok) {
         throw new Error(`status: ${response.status}`);
       }
       const result = await response.json();
-      setUsers(result.users);
-      setTotalPages(result.totalPages);
+      const users = result.users.map((user) => new UserModel(
+        user.id,
+        user.name,
+        user.surname,
+        user.email,
+        user.password,
+        user.phone,
+        user.age,
+        user.country,
+        user.district,
+        user.role,
+        user.createdAt,
+        user.updatedAt
+      ));
+      setUsers(users);
       setTotalCount(result.totalCount);
     } catch (error) {
       console.error("Fetch error:", error);
     }
-  };
+  }, [currentPage, pageSize, searchTerm]);
 
-  const handleSearch = (value) => {
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  const handleSearch = async (value) => {
     setSearchTerm(value);
+    await fetchUsers();
   };
 
   const openModal = (record) => {
@@ -57,6 +77,11 @@ const App = () => {
     <div style={{ padding: '20px' }}>
       <Table
         dataSource={users}
+        locale={{
+          emptyText: (
+            <Empty description="No users found"/>
+          ),
+        }}
         columns={[
           { title: 'Name', dataIndex: 'name', key: 'name' },
           { title: 'Surname', dataIndex: 'surname', key: 'surname' },
@@ -114,9 +139,12 @@ const App = () => {
               <Typography.Title>Usersdot Study Case</Typography.Title>
               <Row justify="space-between" align="middle">
                 <Col>
-                  <Input
-                    placeholder="Search Users"
-                    onChange={(e) => handleSearch(e.target.value)}
+                  <Search
+                    addonBefore={<SearchOutlined />}
+                    placeholder="Search User..."
+                    enterButton="Search"
+                    size="large"
+                    onSearch={(value) => handleSearch(value)}
                   />
                 </Col>
                 <Col>
@@ -131,19 +159,27 @@ const App = () => {
             </Col>
           </div>}
         footer={() => (
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Typography.Text> Showing {users.length} of {totalCount} entries</Typography.Text>
-            <Select defaultValue={10} onChange={handlePageSizeChange} style={{ width: 120 }}>
-              <Option value={2}>2 / page</Option>
-              <Option value={5}>5 / page</Option>
-              <Option value={10}>10 / page</Option>
-              <Option value={15}>15 / page</Option>
-              <Option value={20}>20 / page</Option>
-              <Option value={25}>25 / page</Option>
-              <Option value={50}>50 / page</Option>
-              <Option value={100}>100 / page</Option>
-            </Select>
-          </div>
+          <Row justify="space-between" align="middle">
+            <Col>
+              <Typography.Text> Showing {users.length} of {totalCount} entries</Typography.Text>
+            </Col>
+            <Col>
+              <Select
+                showSearch
+                defaultValue={10}
+                onChange={handlePageSizeChange}
+              >
+                <Option value={2}>2 / page</Option>
+                <Option value={5}>5 / page</Option>
+                <Option value={10}>10 / page</Option>
+                <Option value={15}>15 / page</Option>
+                <Option value={20}>20 / page</Option>
+                <Option value={25}>25 / page</Option>
+                <Option value={50}>50 / page</Option>
+                <Option value={100}>100 / page</Option>
+              </Select>
+            </Col>
+          </Row>
         )}
       />
       <Modal
